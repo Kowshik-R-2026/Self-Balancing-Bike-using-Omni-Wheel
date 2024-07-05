d24232289d6e6dfc8a8e400dc74c1c4d6e288deb
Kowshik <rcskowshik8275@gmail.com>
2024-07-05 13:41:25 +0530

    Self-Balancing-Bike

--- Task0A/pythonAPI/zmqRemoteApi/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.append('..')
-from src.coppeliasim_zmqremoteapi_client import *
-print('warning: import zmqRemoteApi is deprecated. Please use import coppeliasim_zmqremoteapi_client instead.')
